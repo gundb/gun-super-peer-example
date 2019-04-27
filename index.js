46c4dd287b7e9ef8c5f28171612f87e49413edd3
Mark Nadal <mark@accelsor.com>
2019-04-26 17:13:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-var Gun = require('gun')
-var http = require('http')
-var fs = require('fs')
-
-var server = http.createServer((req,res) => {
-	if (Gun.serve(req, res)) { return } // filters gun requests!
-  res.writeHead(200)
-  res.end(fs.readFileSync(__dirname + '/index.html'))
-}).listen(process.env.PORT || process.argv[2] || 8080)
-
-var gun = Gun({web: server})
+console.log(" THIS EXAMPLE IS DEPRECATED \n Please use the 1-click-deploy located at: \n https://github.com/amark/gun#deploy \n Or the example server at
+ \n https://github.com/amark/gun/blob/master/examples/http.js");
